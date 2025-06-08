ac27905d3c60a5b951c7a10566ca7477e2efa05e
mike72312 <mjansbro@gmail.com>
2025-06-08 18:34:05 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,11 +32,6 @@ app.get("/api/availableWrestlers", async (req, res) => {
   }
 });
 
-// Fallback to frontend for all other routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
