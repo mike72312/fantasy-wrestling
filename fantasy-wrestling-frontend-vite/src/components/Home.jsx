3cc2ffb9b3785e98c80d66826f40c32400330612
mike72312 <mjansbro@gmail.com>
2025-06-11 20:59:53 -0400

    Update Home.jsx

--- fantasy-wrestling-frontend-vite/src/components/Home.jsx
+++ fantasy-wrestling-frontend-vite/src/components/Home.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="container">
       <h1>Fantasy Wrestling League</h1>
-      <p>Welcome to the fantasy wrestling site. Choose from the navigation bar above:</p>
-      <ul>
-        <li><Link to="/available-wrestlers">View Available Wrestlers</Link></li>
-        <li><Link to="/standings">View Standings</Link></li>
-        <li><Link to="/transactions">View Transactions</Link></li>
-        <li><Link to="/import-event">Import Event Results</Link></li>
-      </ul>
+      <p>This is the homepage. Navigation is above.</p>
     </div>
   );
 };
