f98b35255878a95557c3a0a0b85851de7939711b
mike72312 <mjansbro@gmail.com>
2025-06-11 22:51:56 -0400

    jhk

--- fantasy-wrestling-frontend-vite/src/App.jsx
+++ fantasy-wrestling-frontend-vite/src/App.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import AvailableWrestlers from './components/AvailableWrestlers';
+import Login from './components/Login';
+import Transactions from './components/Transactions';
+import StandingsAndTransactions from './components/StandingsAndTransactions';
+import TeamRoster from './components/TeamRoster';
+import WrestlerProfile from './components/WrestlerProfile';
+import EventHistory from './components/EventHistory';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <NavBar />
-      <h2>Test Render OK</h2>
-    </BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<StandingsAndTransactions />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/available-wrestlers" element={<AvailableWrestlers />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/standings" element={<StandingsAndTransactions />} />
+          <Route path="/roster/:teamName" element={<TeamRoster />} />
+          <Route path="/wrestler/:wrestlerName" element={<WrestlerProfile />} />
+          <Route path="/events" element={<EventHistory />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
