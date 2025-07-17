4f61dbac6baff2015a905d83ddc7434f6d437e3b
mike72312 <mjansbro@gmail.com>
2025-07-17 12:28:17 -0400

    Update EventSummary.jsx

--- fantasy-wrestling-frontend-vite/src/components/EventSummary.jsx
+++ fantasy-wrestling-frontend-vite/src/components/EventSummary.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./EventSummary.css"; // Optional: use for custom styling
 
 const EventSummary = () => {
   const [data, setData] = useState([]);
